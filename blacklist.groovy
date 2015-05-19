e3f6b7800d55144d0be5500552e32cb5765e30c0
Aaron Long <along@triu.com>
2015-05-18 21:02:48 -0400

    Fix length

--- blacklist.groovy
+++ blacklist.groovy
@@ -1,7 +1,7 @@
 def blacklist = new XmlSlurper().parseText(new File('blacklist.xml').text)
 blacklist.list.num.eachWithIndex { it, idx ->
 	String number = it.text()
-	if (number.length() == 9) {
+	if (number.length() == 10) {
 		number = "1${number}"
 	}
 	println "${number},Callcap: ${Integer.toString(idx+1).padLeft(5, '0')}"
